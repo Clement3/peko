3ca5a23e6e2ff4abdfc604fe8f7e371b0eff6ee9
lucie garcia <lucie2@beweb-16.546O>
2018-09-07 16:34:55 +0200

    Create Migration Professionals & Pages

---
+++ database/migrations/2018_09_07_132222_create_professionals_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfessionalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('professionals', function (Blueprint $table) {
+            $table->unsignedInteger('user_id')->primary();
+            $table->string('social_reason', 100);
+            $table->string('company_name', 100);
+            $table->string('siret', 100);
+            $table->string('tva_intracommunity');
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('professionals');
+    }
+}
