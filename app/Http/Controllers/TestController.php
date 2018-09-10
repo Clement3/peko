6442f25e215e04d3423cf5f0fe7ef8ce180641f1
Clément Besse <clement.besse@gmail.com>
2018-09-10 11:55:13 +0200

    Install Auth - Middleware IsAdmin

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class TestController extends Controller
+{
+    public function test()
+    {
+        $user = User::where('email', 'admin@admin.com')->first();
+
+        return $user->role->slug;
+    }
+}
