9078fff89f7030f978833aba5d3197717478a6e7
Clément Besse <clement.besse@gmail.com>
2018-09-10 14:04:12 +0200

    Routes Resources Users Productsa Sliders Pages

--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,18 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/test', 'TestController@test');
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 
 Route::prefix('admin')->namespace('Admin')->middleware('IsAdmin')->group(function () {
     Route::get('/dashboard', 'DashboardController@index');
+
+    Route::resources([
+        'users' => 'UsersController',
+        'products' => 'ProductsController',
+        'sliders' => 'SlidersController',
+        'pages' => 'PagesController'
+    ]);
 });
