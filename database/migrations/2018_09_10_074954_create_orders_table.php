7f42364856049849da5c09b34348bd793c46ea8b
lucie garcia <lucie2@beweb-16.546O>
2018-09-10 10:09:36 +0200

    create Adresses, Roles, Ordersand Order_products tables

---
+++ database/migrations/2018_09_10_074954_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->boolean('is_received')->defautl('false');
+            $table->float('price');
+            $table->timestamps();
+            $table->unsignedInteger('user_id');
+            $table->string('product_id');
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
