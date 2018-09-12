f9894c350f5fefd921a91eb8ade16f10f44bc512
lucie garcia <lucie2@beweb-16.546O>
2018-09-12 11:33:06 +0200

    Views Sliders/index and Labels/index filled

--- resources/views/admin/labels/index.blade.php
+++ resources/views/admin/labels/index.blade.php
@@ -7,5 +7,52 @@
 
 @include('partials/_alert')
 
+<div class="table-responsive">
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col"># ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Actif</th>
+                <th scope="col">Création</th>
+                <th scope="col">Mis à jour</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($labels as $label)
+            <tr>
+                <th scope="row">{{ $label->id }}</th>
+                <td>{{ $label->name }}</td>
+                <td>{{ $label->created_at }}</td>
+                <td>{{ $label->updated_at }}</td>
+                <td>
+                    @if ($label->is_active) 
+                    <span class="badge badge-info">Oui</span> 
+                    @else
+                    <span class="badge badge-warning">Non</span>
+                    @endif
+                </td>
+                
+                    <a href="{{ route('admin.labels.show', $label) }}">{{ __('View') }}</a>
+                    <a href="{{ route('admin.labels.edit', $label) }}">{{ __('Edit') }}</a>
+                    <a
+                        href="{{ route('admin.labels.destroy', $label }}"
+                        onclick="event.preventDefault();
+                        document.getElementById('delete-form').submit();">
+                        {{ __('Delete') }}
+                    </a>
+   
+                    <form id="delete-form" action="{{ route('admin.labels.destroy', $label) }}" method="POST" style="display: none;">
+                        @csrf
+                        @method('DELETE')
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+
+{{ $labels->links() }}
 
 @endsection
