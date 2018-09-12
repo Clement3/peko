710591e2ec457a157ebfcb5a4024f2b17e534501
ClementPeyronnet <clement.peyronnet@hotmail.com>
2018-09-12 11:14:45 +0200

    Admin newsletters create table

--- resources/views/admin/newsletters/index.blade.php
+++ resources/views/admin/newsletters/index.blade.php
@@ -1,28 +1,49 @@
-@extends('layouts.app')
+@extends('layouts.admin')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Users</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    @foreach ($users as $user)
-                        {{ $user->name }}
-                        {{ $user->email }}
-                    @endforeach 
-
-                    {{ $users->links() }}
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
+    <h1 class="h2">Newsletter</h1>
 </div>
+
+@include('partials/_alert')
+
+<div class="table-responsive">
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col"># ID</th>
+                <th scope="col">E-mail</th>
+                <th scope="col">Créer à</th>
+                <th scope="col">Mise à jour</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($newsletters as $newsletter)
+            <tr>
+                <th scope="row">{{ $newsletter->id }}</th>
+                <td>{{ $newsletter->email }}</td>
+                <td>{{ $newsletter->created_at }}</td>
+                <td>{{ $newsletter->updated_at }}</td>
+                <td>
+                    <a href="{{ route('admin.newsletters.show', $newsletter) }}">{{ __('View') }}</a>
+                    <a href="{{ route('admin.newsletters.edit', $newsletter) }}">{{ __('Edit') }}</a>
+                    <a
+                        href="{{ route('admin.newsletters.destroy', $newsletter) }}"
+                        onclick="event.preventDefault();
+                        document.getElementById('delete-form').submit();">
+                        {{ __('Delete') }}
+                    </a>
+   
+                    <form id="delete-form" action="{{ route('admin.newsletters.destroy', $newsletter) }}" method="POST" style="display: none;">
+                        @csrf
+                        @method('DELETE')
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+
+{{ $newsletters->links() }}
 @endsection
