52c131ac70b648a9fe3143b563c668e8ca9a07b5
NicolasLOJO <niko34310@gmail.com>
2018-09-13 15:29:21 +0200

    fix navbar & database

--- resources/views/components/products_nav.blade.php
+++ resources/views/components/products_nav.blade.php
@@ -1,5 +1,5 @@
 <li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" href="https://bootstrapthemes.co" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         Catalogue
     </a>
     <ul class="dropdown-menu dropright" aria-labelledby="navbarDropdownMenuLink">
@@ -7,7 +7,7 @@
         <li><a class="dropdown-item dropdown-toggle" href="#">{{ $category->name }}</a>
             <ul class="dropdown-menu">
             @foreach($category->varieties as $variety)
-                <li><a class="dropdown-item" href="#">{{ $variety->name }}</a></li>
+                <li class="align-middle d-inline"><a class="align-middle dropdown-item" href="#"><img src="{{ asset('image/categorie/'.$variety->picture) }}" alt="" style="width: 40px; height: 40px; margin-right: 20px;">{{ $variety->name }}</a></li>
             @endforeach
             </ul>
         </li>
