a497bc3cfeece710a1e398b59e8546d6eecf1716
NicolasLOJO <niko34310@gmail.com>
2018-09-13 16:02:05 +0200

    2nd Fix Navbar

--- resources/views/components/products_nav.blade.php
+++ resources/views/components/products_nav.blade.php
@@ -3,11 +3,12 @@
         Catalogue
     </a>
     <ul class="dropdown-menu dropright" aria-labelledby="navbarDropdownMenuLink">
+        <li class="dropdown-item"><a href="/catalogue">Tout les produits</a></li>
         @foreach($cat as $category)
         <li><a class="dropdown-item dropdown-toggle" href="#">{{ $category->name }}</a>
             <ul class="dropdown-menu">
             @foreach($category->varieties as $variety)
-                <li class="align-middle d-inline"><a class="align-middle dropdown-item" href="#"><img src="{{ asset('image/categorie/'.$variety->picture) }}" alt="" style="width: 40px; height: 40px; margin-right: 20px;">{{ $variety->name }}</a></li>
+                <li class="align-middle d-inline"><a class="align-middle dropdown-item" href="/catalogue/{{ $category->slug }}/{{ $variety->slug }}"><img src="{{ asset('image/categorie/'.$variety->picture) }}" style="width: 40px; height: 40px; margin-right: 20px;">{{ $variety->name }}</a></li>
             @endforeach
             </ul>
         </li>
